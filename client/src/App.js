0b3ce275c4ec731d51e2377c1ab6daddaa115e3c
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-04-14 21:34:07 +0530

    feat: added payment gateway integration

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const paymentHandler = (response) => {
+    console.log(response);
+    alert(response.razorpay_payment_id);
+    alert(response.razorpay_order_id);
+    alert(response.razorpay_signature);
+  };
+
+  const razorpayPayment = async (amount) => {
+    const { data } = await axios.post("/createOrder", {
+      amount: amount,
+      notes: {
+        user: "Anand Shirbhaiyye",
+        item: "Men-Tshirt",
+      },
+    });
+
+    const orderId = data.order.id;
+
+    const options = {
+      key: "rzp_test_Hx1u2oka1Tudh0",
+      amount: amount * 100,
+      currency: "INR",
+      name: "Anand_Ujwal",
+      description: "xyz",
+      image: "",
+      order_id: orderId,
+      handler: paymentHandler,
+      prefill: {
+        name: "Gaurav Kumar",
+        email: "gaurav.kumar@example.com",
+        contact: "9000090000",
+      },
+      notes: {
+        address: "Razorpay Corporate Office",
+      },
+      theme: {
+        color: "#0dcaf0",
+      },
+    };
+
+    const rzp = new window.Razorpay(options);
+    rzp.open();
+
+    rzp.on("payment.failed", function (response) {
+      alert("Payment Failed");
+      console.log(response);
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="text-center mt-3">Payment Integration</h1>
+
+      <div className="container">
+        <div class="card payment-integration-card mt-5 shadow p-3 mb-5 rounded">
+          <img
+            src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1678715534_2775447.jpg?format=webp&w=360&dpr=1.5"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title text-center">Men T-Shirts</h5>
+            <p class="card-text">
+              Material & Care: Premium Heavy Gauge Fabric 80% Cotton 20%
+              Polyester Machine Wash
+            </p>
+            <p class="card-text">₹ 1000.00</p>
+            <div className="text-center">
+              <button
+                class="btn btn-warning mt-2 p-2"
+                onClick={() => {
+                  razorpayPayment(1000);
+                }}
+              >
+                Buy Now
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
