37286c06d952d39fcce72fd7800d37492618ff1d
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-04-14 13:58:17 +0530

    create order api

--- server/index.js
+++ server/index.js
@@ -1,25 +1,45 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import Razorpay from "razorpay";
 
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-const connectDB = async()=>{
-    const connect = await mongoose.connect(process.env.MONGODB_URI);
-    if(connect){
-        console.log("MongoDB is Connected...👋")
-    }
-}
+const instance = new Razorpay({
+  key_id: process.env.KEY_ID,
+  key_secret: process.env.KEY_SECRET,
+});
+
+const connectDB = async () => {
+  const connect = await mongoose.connect(process.env.MONGODB_URI);
+  if (connect) {
+    console.log("MongoDB is Connected...👋");
+  }
+};
 connectDB();
 
-
-
-
+app.post("/createOrder", async(req, res) => {
+    const { amount, notes } = req.body;
+  
+    const order = await instance.orders.create({
+      amount: amount * 100,
+      currency: "INR",
+      receipt: "Anand_Ujwal_"+Math.floor(Math.random() * 1000000),
+      notes: notes
+    })
+  
+    res.json({
+      success: true,
+      message: "Order created successfully...",
+      order: order
+    });
+  })
+  
 
 app.listen(PORT, () => {
-    console.log(`The server is Running on Port ${PORT}🙏`);
+  console.log(`The server is Running on Port ${PORT}🙏`);
 });
